0b7d1cf17162495d8c790de84e9ae72ca9cf3f66
Bhasker23 <shrimalibhaskar2398@gmail.com>
2022-10-08 22:07:19 +0530

    Parent Exception added

--- Blog_Application/src/main/java/com/masai/exceptionClasses/ErrorDetails.java
+++ Blog_Application/src/main/java/com/masai/exceptionClasses/ErrorDetails.java
@@ -1,6 +1,18 @@
 package com.masai.exceptionClasses;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ErrorDetails {
 
+	private LocalDate date;
+	private String message;
+	private String description;
 	
 }
--- Blog_Application/src/main/java/com/masai/exceptionHandler/GlobalExceptionHandler.java
+++ Blog_Application/src/main/java/com/masai/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,30 @@
 package com.masai.exceptionHandler;
 
+import java.time.LocalDate;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import com.masai.exceptionClasses.ErrorDetails;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> parentException(Exception exception , WebRequest wr){
+		
+		ErrorDetails er = new ErrorDetails();
+		
+		er.setMessage(exception.getMessage());
+		er.setDescription(wr.getDescription(false));
+		er.setDate(LocalDate.now());
+		
+		return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+		
+		
+	}
+	
 }
